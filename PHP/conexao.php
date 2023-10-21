dd6cf941d89144c1ccf7dcf890b4ae9830753c06
maluiiii <143665235+maluiiii@users.noreply.github.com>
2023-10-21 13:35:44 -0300

    Add files via upload

---
+++ PHP/conexao.php
@@ -0,0 +1,11 @@
+<?php
+    $server = "localhost";
+    $user = "root";
+    //$password = "Seemg@123";
+    $password = "";
+    $db = "sasvi";
+    
+   $conn =  mysqli_connect($server, $user, $password, $db);
+
+    //$teste = "Conexão realizada com sucesso!";
+?>
